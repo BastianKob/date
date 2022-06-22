130a646060d43512b454f6d82f230fdb7f6d9973
BastianKob <105808602+BastianKob@users.noreply.github.com>
2022-06-22 18:30:43 +0200

    Umsetzung Anforderungen 3

--- src/main/java/com/example/demo/DateApplication.java
+++ src/main/java/com/example/demo/DateApplication.java
@@ -3,8 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 @SpringBootApplication
 @RestController
@@ -20,8 +23,14 @@ public class DateApplication {
 	}
 
 	@GetMapping("api/date")
-	public String get_current_date(){
-		return LocalDate.now().toString();
+	public String get_current_date(@RequestParam(name = "format", defaultValue = "") String format){
+		if(format.equals("")) {
+			return LocalDate.now().toString();
+		}
+		else{
+			DateTimeFormatter fmt = DateTimeFormatter.ofPattern(format);
+			return LocalDate.now().format(fmt);
+		}
 	}
 
 }
