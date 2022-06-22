b284c8513bb0ba8a41a89d3d79ee98c6ca6b5b8e
BastianKob <105808602+BastianKob@users.noreply.github.com>
2022-06-22 18:20:22 +0200

    Anforderung umgesetzt

--- src/main/java/com/example/demo/DateApplication.java
+++ src/main/java/com/example/demo/DateApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.*;
 
 @SpringBootApplication
+@RestController
 public class DateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DateApplication.class, args);
+	}
+
+	@GetMapping("/api/time")
+	public String get_current_time(){
+		return LocalTime.now().toString();
 	}
 
 }
